d4ac5d414b71f7f19176daa79ec95da0bf113771
inmanturbo <inman.turbo@gmail.com>
2020-09-01 22:44:49 -0400

    wip

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -9,84 +9,310 @@
     $dotenv->load();
 
     /**
-     *  Password for "ansible" user on all galera nodes
+     *  Password for "admin" user on all galera nodes
+     *  current scripts require passwordless sudo
      */
-    $passwd=$_ENV['GALERA_PASS'];
+    $adminPasswd=$_ENV['ADMIN_PASSWORD'];
+
+    /**
+     *  Password for root mysql account on all galera nodes
+     */
+    $mysqlRootPasswd=$_ENV['MYSQL_ROOT_PASSWORD'];
+
+    /**
+     *  Password for "admin" username on all galera nodes
+     *  current scripts require passwordless sudo
+     */
+    $adminUsername=$_ENV['ADMIN_USERNAME'];
+
+    /**
+     *  MariaDb version to be used
+     *  e.g 10.4
+     */
+     $mariaDbVersion=$_ENV['MARIADB_VERSION'];
+
+     /**
+     *  Yum/DNF releasever
+     *  current scripts only support centos/rhel 8
+     *  e.g rhel8-amd64
+     */
+     $releaseVer=$_ENV['RELEASE_VER'];
+
+    /**
+    * Define ssh logins for galera nodes
+    * user@host.domain.tld
+    */
     $db1=$_ENV['GALERA_NODE_ONE'];
     $db2=$_ENV['GALERA_NODE_TWO'];
     $db3=$_ENV['GALERA_NODE_THREE'];
+
+
+    /**
+    * Comma seperated list of hostnames
+    * or ips for galera nodes
+    */
+    $galeraHostList=$_ENV['CLUSTER_HOST_LIST'];
+
+    $galeraHostOne=explode(',',$galeraHostList)[0];
+    $galeraHostTwo=explode(',',$galeraHostList)[1];
+    $galeraHostThree=explode(',',$galeraHostList)[2];
+
+    /**
+    * Optional. Most critical database 
+    * e.g. my_production_db_name
+    */
     $primaryDb=$_ENV['PRIMARY_DB'];
+
+    /**
+    * Optional backup method to simple https file
+    * server on network with curl support. 
+    * e.g. fileserver.local
+    */
     $simpleFileServerUrl=$_ENV['SIMPLE_FILE_SERVER_URL'];
+
+    /**
+    * ssh login for cluster manager
+    * or single node hypervisor
+    * requires libvirt
+    * uses virsh command
+    * e.g. root@my_virt_host.local
+    */
     $libvirtWorker=$_ENV['LIBVIRT_WORKER'];
 
     /**
+     *  List all servers here
      *  All nodes' vm names in libvirt must be
      *  be same as their key in server array
      */
     $servers = [
+
         'db1'   => $db1,
         'db2'   => $db2,
         'db3'   => $db3,
         'libvirt' => $libvirtWorker,
         'local' => '127.0.0.1',
     ];
-    
-    $dbNodes = [
+
+    /**
+    * Which servers are meant to be
+    * in the Galera Cluster
+    */
+    $galeraNodes = [
         'db1','db2','db3'
     ];
-    $dbHosts = [
+
+    /**
+    * ssh connections for galera nodes
+    */
+    $galeraSSHConnections = [
         $db1,$db2,$db3
     ];
 
+    /**
+    * For timestamp versioning mysql dumps
+    */
     $now = \Carbon\Carbon::now()->format('m_d_Y-h');
 
-    $installDbCommand='echo "[mariadb]" > /etc/yum.repos.d/mariadb.repo && echo "name = MariaDB" >> /etc/yum.repos.d/mariadb.repo && echo "baseurl = http://yum.mariadb.org/10.4/rhel8-amd64" >> /etc/yum.repos.d/mariadb.repo && echo "gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB" >> /etc/yum.repos.d/mariadb.repo && echo "gpgcheck=1" >> /etc/yum.repos.d/mariadb.repo && dnf makecache && dnf install -y galera-4 && dnf install -y MariaDB-server MariaDB-client --disablerepo=AppStream'
+    $installDbCommand='echo "[mariadb]" |sudo tee /etc/yum.repos.d/mariadb.repo > /dev/null '.
+                       '&& echo "name = MariaDB"|sudo tee -a /etc/yum.repos.d/mariadb.repo >/dev/null '. 
+                       '&& echo "baseurl = http://yum.mariadb.org/'.$mariaDbVersion.'/'.$releaseVer.'"|sudo tee -a /etc/yum.repos.d/mariadb.repo > /dev/null '.
+                       '&& echo "gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB"|sudo tee -a /etc/yum.repos.d/mariadb.repo > /dev/null '.
+                       '&& echo "gpgcheck=1"|sudo tee -a /etc/yum.repos.d/mariadb.repo > /dev/null '.
+                       '&& sudo dnf makecache '.
+                       '&& sudo dnf install -y galera-4 '.
+                       '&& sudo dnf install -y MariaDB-server MariaDB-client --disablerepo=AppStream';
+    
+    $nodeOneTemplate=<<<EOF
+[mysqld]
+binlog_format=ROW
+default-storage-engine=innodb
+innodb_autoinc_lock_mode=2
+bind-address=0.0.0.0
+
+# Galera Provider Configuration
+wsrep_on=ON
+wsrep_provider=/usr/lib64/galera-4/libgalera_smm.so
+
+# Galera Cluster Configuration
+wsrep_cluster_name="test_cluster"
+wsrep_cluster_address="gcomm://$galeraHostList"
+
+# Galera Synchronization Configuration
+wsrep_sst_method=rsync
+
+# Galera Node Configuration
+wsrep_node_address="$galeraHostOne"
+wsrep_node_name="db1"
+EOF;
+
+    $nodeTwoTemplate=<<<EOF
+[mysqld]
+binlog_format=ROW
+default-storage-engine=innodb
+innodb_autoinc_lock_mode=2
+bind-address=0.0.0.0
+
+# Galera Provider Configuration
+wsrep_on=ON
+wsrep_provider=/usr/lib64/galera-4/libgalera_smm.so
+
+# Galera Cluster Configuration
+wsrep_cluster_name="test_cluster"
+wsrep_cluster_address="gcomm://$galeraHostList"
+
+# Galera Synchronization Configuration
+wsrep_sst_method=rsync
+
+# Galera Node Configuration
+wsrep_node_address="$galeraHostTwo"
+wsrep_node_name="db3"
+EOF;
+
+    $nodeThreeTemplate=<<<EOF
+[mysqld]
+binlog_format=ROW
+default-storage-engine=innodb
+innodb_autoinc_lock_mode=2
+bind-address=0.0.0.0
+
+# Galera Provider Configuration
+wsrep_on=ON
+wsrep_provider=/usr/lib64/galera-4/libgalera_smm.so
+
+# Galera Cluster Configuration
+wsrep_cluster_name="test_cluster"
+wsrep_cluster_address="gcomm://$galeraHostList"
+
+# Galera Synchronization Configuration
+wsrep_sst_method=rsync
+
+# Galera Node Configuration
+wsrep_node_address="$galeraHostThree"
+wsrep_node_name="db3"
+EOF;
+
+$galeraHostConfigArray=[
+
+    'db1' => ['template' => $nodeOneTemplate, 'hostname' => $galeraHostOne],
+    'db2' => ['template' => $nodeTwoTemplate, 'hostname' => $galeraHostTwo],
+    'db3' => ['template' => $nodeThreeTemplate, 'hostname' => $galeraHostThree],
+];
+
 @endsetup
 
 @servers($servers)
 
+@story('setup-cluster')
+
+    install-keys
+    configure-passwordless-sudo
+    update-nodes
+    install-mariadb-galera
+    install-tools
+    enable-mariadbs
+    start-mariadbs
+    set-root-mysql-passwd
+    configure-mariadb-galera-node-one
+    configure-mariadb-galera-node-two
+    configure-mariadb-galera-node-three
+    setup-firewall
+    configure-selinux
+    stop-mariadbs
+    first-start1
+    first-start-nodes
+    create-enable-selinux-policies
+    activate-selinux-policies
+    secure-nodes
+
+@endstory
+
+@story('up-cluster')
+
+    start-nodes
+    wait-for-cluster
+    start1
+    wait-for-one
+    join-nodes
+    enable-mariadbs
+
+@endstory
+
+@story('down-cluster')
+
+    stop-mariadbs
+    disable-mariadbs
+    shutdown-nodes
+
+@endstory
+
+@story('start-cluster')
+
+    start1
+    wait-for-one
+    join-nodes
+
+@endstory
+
 @task('install-keys',['on'=> ['local']])
-    @foreach($dbHosts as $host)
+    @foreach(array_values($servers) as $host)
         ssh-copy-id {{$host}}
     @endforeach
 @endtask
 
-@task('start-dbs',['on'=> ['libvirt']])
-    @foreach($dbNodes as $node)
-        virsh start {{$node}}
-    @endforeach
+@task('configure-passwordless-sudo',['on'=> $galeraNodes])
+echo "{{$adminPasswd}}"|sudo -S echo "hello sudo" && echo '{{$adminUsername}} ALL=(ALL) NOPASSWD: ALL'|sudo tee -a /etc/sudoers
 @endtask
 
-
-@task('reboot-dbs',['on'=> ['libvirt']])
-@foreach($dbNodes as $node)
-    virsh reboot {{$node}}
-@endforeach
-@endtask
-
-@task('install-dbs',['on'=> $dbNodes])
-    {{$installDbCommand}}
-
-@endtask
-
-@task('install-tools',['on'=> $dbNodes])
-    sudo dnf install -y rsync policycoreutils-python-utils jq
-@endtask
-
-@task('update-dbs',['on'=> $dbNodes])
+@task('update-nodes',['on'=> $galeraNodes])
     sudo dnf update -y
 @endtask
 
-@task('enable-dbs',['on'=> $dbNodes])
+@task('install-mariadb-galera',['on'=> $galeraNodes])
+    {{$installDbCommand}}
+@endtask
+
+@task('install-tools',['on'=> $galeraNodes])
+    sudo dnf install -y rsync policycoreutils-python-utils jq
+@endtask
+
+{{-- SERVICES --}}
+@task('enable-mariadbs',['on'=> $galeraNodes])
    sudo systemctl enable mariadb
 @endtask
 
+@task('disable-mariadbs',['on'=> $galeraNodes])
+    sudo systemctl disable mariadb
+@endtask
 
-@task('enable-cockpit',['on'=> $dbNodes])
+@task('start-mariadbs',['on'=> $galeraNodes])
+   sudo systemctl start mariadb
+@endtask
+
+@task('stop-mariadbs',['on'=> ['db3','db2','db1']])
+    sudo systemctl stop mariadb
+@endtask
+
+@task('enable-cockpit',['on'=> $galeraNodes])
    sudo systemctl enable --now cockpit.socket
 @endtask
 
-@task('setup-firewall',['on'=> $dbNodes])
+@task('set-root-mysql-passwd',['on'=> $galeraNodes])
+    sudo mysqladmin --user=root password "{{$mysqlRootPasswd}}"
+@endtask
+
+@task('configure-mariadb-galera-node-one',['on'=> ['db1']])
+    echo '{{$nodeOneTemplate}}'|sudo tee /etc/my.cnf.d/galera.cnf
+@endtask
+
+@task('configure-mariadb-galera-node-two',['on'=> ['db2']])
+    echo '{{$nodeTwoTemplate}}'|sudo tee /etc/my.cnf.d/galera.cnf 
+@endtask
+
+@task('configure-mariadb-galera-node-three',['on'=> ['db3']])
+    echo '{{$nodeThreeTemplate}}'|sudo tee /etc/my.cnf.d/galera.cnf 
+@endtask
+
+@task('setup-firewall',['on'=> $galeraNodes])
     sudo firewall-cmd --permanent --add-port=3306/tcp
     sudo firewall-cmd --permanent --add-port=4567/tcp
     sudo firewall-cmd --permanent --add-port=4568/tcp
@@ -95,7 +321,7 @@
     sudo firewall-cmd --reload
 @endtask
 
-@task('selinux-conf',['on'=> $dbNodes])
+@task('configure-selinux',['on'=> $galeraNodes])
     #sudo semanage port -a -t mysqld_port_t -p tcp 4567
     sudo semanage port -a -t mysqld_port_t -p udp 4567
     sudo semanage port -a -t mysqld_port_t -p tcp 4568
@@ -103,74 +329,55 @@
     sudo semanage permissive -a mysqld_t
 @endtask
 
-@task('stop-dbs',['on'=> $dbNodes])
-    sudo systemctl stop mariadb
+@task('first-start1',['on' => ['db1']])
+    sudo galera_new_cluster
+    mysql -u root -p{{$mysqlRootPasswd}} -e "CREATE DATABASE selinux;CREATE TABLE selinux.selinux_policy (id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id));INSERT INTO selinux.selinux_policy VALUES ();"
 @endtask
 
+@task('first-start-nodes',['on'=> ['db2','db3']])
+    sudo systemctl start mariadb
+@endtask
 
-@task('policies',['on'=> $dbNodes])
-    mysql -u root -p{{$passwd}} -e 'INSERT INTO selinux.selinux_policy VALUES ();'
+@task('create-enable-selinux-policies',['on'=> $galeraNodes])
+    mysql -u root -p{{$mysqlRootPasswd}} -e 'INSERT INTO selinux.selinux_policy VALUES ();'
     sudo grep mysql /var/log/audit/audit.log | sudo audit2allow -M Galera
 @endtask
 
-@task('activate-policies',['on'=> $dbNodes])
+@task('activate-selinux-policies',['on'=> $galeraNodes])
     sudo semodule -i Galera.pp
 @endtask
 
-@task('secure-nodes',['on'=> $dbNodes])
+@task('secure-nodes',['on'=> $galeraNodes])
     sudo semanage permissive -d mysqld_t
 @endtask
 
-
-@story('start-cluster')
-    start1
-    start-nodes
-@endstory
-
-@task('start1',['on' => ['db1']])
-    sudo galera_new_cluster
-    mysql -u root -p{{$passwd}} -e "SHOW STATUS LIKE 'wsrep_cluster_size'"
+@task('start-nodes',['on'=> ['libvirt']])
+    @foreach($galeraNodes as $node)
+        virsh start {{$node}}
+    @endforeach
 @endtask
 
-
-@task('start-nodes',['on'=> ['db2','db3']])
-    sudo systemctl start mariadb
-    sleep 5 && mysql -u root -p{{$passwd}} -e "SHOW STATUS LIKE 'wsrep_cluster_size'"
-@endtask
-
-@task('ping-nodes',['on'=> ['db1','db2','db3']])
-    mysql -u root -p{{$passwd}} -e "SHOW STATUS LIKE 'wsrep_cluster_size'"
-@endtask
-
-
-@story('down-cluster')
-    stop-nodes
-    disable-dbs
-    shutdown-dbs
-@endstory
-
-@task('disable-dbs',['on'=> $dbNodes])
-    sudo systemctl disable mariadb
-@endtask
-
-@task('shutdown-dbs',['on'=> ['libvirt']])
-    @foreach($dbNodes as $node)
+@task('shutdown-nodes',['on'=> ['libvirt']])
+    @foreach($galeraNodes as $node)
         virsh shutdown {{$node}}
     @endforeach
 @endtask
 
-@task('stop-nodes',['on'=> ['db3','db2','db1']])
-    sudo systemctl stop mariadb
+@task('kill-nodes',['on'=> ['libvirt']])
+    @foreach($galeraNodes as $node)
+        virsh destroy {{$node}}
+    @endforeach
 @endtask
 
-@story('up-cluster')
-    start-dbs
-    wait-for-cluster
-    start1
-    wait-for-one
-    start-nodes
-    enable-dbs
-@endstory
+@task('reboot-nodes',['on'=> ['libvirt']])
+    @foreach($galeraNodes as $node)
+        virsh reboot {{$node}}
+    @endforeach
+@endtask
+
+@task('list-nodes',['on'=> ['libvirt']])
+        virsh list --all|grep db
+@endtask
 
 @task('wait-for-cluster', ['on' => ['local']])
     until ssh {{$db3}} "echo up"
@@ -179,30 +386,41 @@
         done
 @endtask
 
+@task('start1',['on' => ['db1']])
+    sudo galera_new_cluster
+    mysql -u root -p{{$mysqlRootPasswd}} -e "SHOW STATUS LIKE 'wsrep_cluster_size'"
+@endtask
+
 @task('wait-for-one', ['on' => ['local']])
     sleep 10
 @endtask
 
-@task('stop-nodes',['on'=> ['db3','db2','db1']])
-    sudo systemctl stop mariadb
+@task('join-nodes',['on'=> ['db2','db3']])
+    sudo systemctl start mariadb
+    sleep 5 && mysql -u root -p{{$mysqlRootPasswd}} -e "SHOW STATUS LIKE 'wsrep_cluster_size'"
+@endtask
+
+@task('ping-nodes',['on'=> ['db1','db2','db3']])
+    mysql -u root -p{{$mysqlRootPasswd}} -e "SHOW STATUS LIKE 'wsrep_cluster_size'"
 @endtask
 
 @task('force-bootstrap',['on'=> ['db1']])
     sudo sed -i -e 's/safe_to_bootstrap: 0/safe_to_bootstrap: 1/' /var/lib/mysql/grastate.dat
 @endtask
 
+{{-- EXAMPLE BACKUP TASK --}}
 @task('backup-db',['on'=> ['db1']])
-    mysql -u root -p{{$passwd}} --execute "SET GLOBAL wsrep_desync = ON";
-    sudo mysqldump -u root -p{{$passwd}} --flush-logs --databases {{$primaryDb}} |sudo tee /backups/{{$primaryDb}}-{{$now}}.sql > /dev/null;
-    sudo mysqldump -u root -p{{$passwd}} --flush-logs --all-databases |sudo tee /backups/db-backup-all-{{$now}}.sql > /dev/null;
-    mysql -u root -p{{$passwd}} --execute "SET GLOBAL wsrep_desync = OFF";
+    mysql -u root -p{{$mysqlRootPassword}} --execute "SET GLOBAL wsrep_desync = ON";
+    sudo mysqldump -u root -p{{$mysqlRootPassword}} --flush-logs --databases {{$primaryDb}} |sudo tee /backups/{{$primaryDb}}-{{$now}}.sql > /dev/null;
+    sudo mysqldump -u root -p{{$mysqlRootPassword}} --flush-logs --all-databases |sudo tee /backups/db-backup-all-{{$now}}.sql > /dev/null;
+    mysql -u root -p{{$mysqlRootPassword}} --execute "SET GLOBAL wsrep_desync = OFF";
     curl -sF file=@/backups/{{$primaryDb}}-{{$now}}.sql {{$simpleFileServerUrl}}  > ~/.backup-report-{{$primaryDb}}-{{$now}}.log;
     curl -sF file=@/backups/db-backup-all-{{$now}}.sql {{$simpleFileServerUrl}} > ~/.backup-report-all-{{$now}}.log;
     if [[ $(jq '.success' ~/.backup-report-{{$primaryDb}}-{{$now}}.log) = "true" ]]; then sudo rm /backups/{{$primaryDb}}-{{$now}}.sql;fi;
     if [[ $(jq '.success' ~/.backup-report-all-{{$now}}.log) = "true" ]]; then sudo rm /backups/db-backup-all-{{$now}}.sql;fi;
 @endtask
 
-@task('install-script', ['on' => ['local']])
+@task('install-task', ['on' => ['local']])
     echo '#!/bin/bash'|tee ${PWD}/bin/{{$task}};
     echo "target_dir=${PWD}"|tee -a ${PWD}/bin/{{$task}};
     echo 'previous_dir=${PWD}'|tee -a ${PWD}/bin/{{$task}};
@@ -211,6 +429,4 @@
     echo 'cd $previous_dir'|tee -a ${PWD}/bin/{{$task}};
 @endtask
 
-@task('list-nodes',['on'=> ['libvirt']])
-        virsh list --all|grep db
-@endtask
+
